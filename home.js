acd53501f74e9fc6e749743e0956cfebe3c11141
KevinBaoPhamMain <baofan2007@gmail.com>
2026-07-30 15:54:20 +0700

    Add in JS

---
+++ home.js
@@ -0,0 +1,12 @@
+let ListAchievements = ["ACSL", "Teacher, I am not gaming!"]
+let ListDescription = ["ABCSCIHASCHACHAHDWLIDHAWDHLIADI", "First game made in Unity"]
+let ListImage = ["Assets/Achievements/PHAMGIABAO.jpg", "Assets/Achievements/TEACHER.png"]
+
+let BlankHTML = ""
+
+for (let i = 0 ; i < ListAchievements.length; i++){
+    let BasedExam = `<div class='AcBlock'> \n <img src='${ListImage[i]}'> \n <p>${ListAchievements[i]}</p> \n <p>${ListDescription[i]}</p> \n </div>`
+    BlankHTML += BasedExam
+}
+    
+document.getElementById("AchievementBlocks").innerHTML = BlankHTML
